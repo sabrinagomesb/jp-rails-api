6b6a5345f0b9271a649a19ff88f8cca7072e3061
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-18 20:28:27 -0300

    adiciona dados com gem faker no db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Inserting contact...'
+100.times do
+  Contact.find_or_create_by(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    birthdate: Faker::Date.birthday(min_age: 18, max_age: 65)
+  )
+end
+puts 'Contact inserted!'
