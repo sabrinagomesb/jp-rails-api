a2bf23f44c0494e95dc90034a84963b19bf5ecc6
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-21 20:51:47 -0300

    adiciona metodos para personalizar o 'render json'

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,18 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+
+  def kind_description
+    self.kind.description
+  end
+
+  def as_json()
+    super(
+      root: true,
+      except: [:kind_id],
+      methods: [:kind_description]
+      # include: { kind: { only: :description }
+    )
+
+  end
+
 end
