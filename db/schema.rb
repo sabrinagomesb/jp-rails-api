fd8cb7f1b999752516ee46feb18edec95362ad0a
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-21 19:57:42 -0300

    adiciona referencia de Kind para Contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_18_212027) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_21_222734) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.date "birthdate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "kind_id"
+    t.index ["kind_id"], name: "index_contacts_on_kind_id"
   end
 
+  create_table "kinds", force: :cascade do |t|
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "contacts", "kinds"
 end
