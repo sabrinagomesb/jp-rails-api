b5694156c3fad64371bc0b5434a2de9616a1a08f
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-24 20:09:54 -0300

    gera model Address

---
+++ db/migrate/20230824200736_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :addresses do |t|
+      t.string :street
+      t.string :city
+      t.string :state
+      t.references :contact, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
