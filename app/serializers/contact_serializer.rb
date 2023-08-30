3116f9d3c6074626c17234cf39981bb7013ba05e
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-30 17:55:28 -0300

    adiciona nó para links no JSON

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,14 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate, :author
 
-  belongs_to :kind, optional: true
+  belongs_to :kind, optional: true do
+    link(:related) { kind_url(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
+
+  link(:self) { contact_url(object.id) }
 
  def author
   'Sabrina Gomes'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,11 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  # set default_url_options[:host]
+  Rails.application.routes.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
