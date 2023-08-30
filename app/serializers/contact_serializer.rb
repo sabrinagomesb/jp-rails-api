d5c4a80782e7b049e3bdca409c3573762a625181
Sabrina Gomes <sabrinagbdev@gmail.com>
2023-08-30 16:35:40 -0300

    adiciona atributo e relacionamentos

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,5 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :birthdate
+  attributes :id, :name, :email, :birthdate, :author
+
+  belongs_to :kind, optional: true
+  has_many :phones
+  has_one :address
+
+ def author
+  'Sabrina Gomes'
+ end
 
   def attributes(*args)
     h = super(*args)
